2b1712100853c69edc482215dccfe85dfd0ec9ea
Rizqi Bagus Andrean <bagus.ok55@gmail.com>
2024-01-14 11:16:42 +0700

    add: base layout for user

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -5,7 +5,13 @@ export default function UserLayout({
 }) {
   return (
     <>
-      <h1>Layout</h1>
+      <main className="w-dvw min-h-dvh md:flex ">
+        <div className="hidden md:block md:w-80 lg:w-96 bg-slate-200 flex-initial"></div>
+        <div className="flex-auto flex flex-col">
+          <header className="h-16 md:h-20 bg-white inline-flex items-center md:border-b-[0.5px] md:border-slate-300 shadow-sm"></header>
+          <div className="p-6 md:p-8">{children}</div>
+        </div>
+      </main>
     </>
   );
 }
