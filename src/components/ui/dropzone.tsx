c1f2345eb4da141312e011b1d483daf8d50492a6
Rizqi Bagus Andrean <bagus.ok55@gmail.com>
2024-01-20 11:12:26 +0700

    add(jolly ui): dropzone

---
+++ src/components/ui/dropzone.tsx
@@ -0,0 +1,20 @@
+"use client"
+
+import * as React from "react"
+import { DropZone, DropZoneProps } from "react-aria-components"
+
+import { cn } from "@/lib/utils"
+
+const _DropZone = ({ className, ...props }: DropZoneProps) => (
+  <DropZone
+    className={(values) =>
+      cn(
+        "flex h-[150px] w-[300px] flex-col items-center justify-center gap-2 rounded-md border border-dashed text-sm data-[drop-target]:border-solid data-[drop-target]:border-primary data-[drop-target]:bg-accent",
+        typeof className === "function" ? className(values) : className
+      )
+    }
+    {...props}
+  />
+)
+
+export { _DropZone as DropZone }
